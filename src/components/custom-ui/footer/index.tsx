ca8681fede8dbd7866d42019f106c2a032296319
Đorđe Matić <djmatic@agfbl.org>
2024-07-20 12:59:17 +0200

    implement footer - first iteration

---
+++ src/components/custom-ui/footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="flex flex-col items-center py-4 text-gray-600">
+      <span className="text-lg italic">mobilnost.ba</span>
+      <span className="border text-xs">
+        Copyright &#169; 2024 All right reserved!
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
