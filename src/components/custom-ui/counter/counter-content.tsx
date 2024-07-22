3842bf4ec770530119414913cdf7c17a0c2347b5
Đorđe Matić <djmatic@agfbl.org>
2024-07-22 09:56:29 +0200

    counter-content: small styling modifications

--- src/components/custom-ui/counter/counter-content.tsx
+++ src/components/custom-ui/counter/counter-content.tsx
@@ -5,8 +5,8 @@ type CounterContentProps = {
 
 const CounterContent = ({ count, measure }: CounterContentProps) => {
   return (
-    <div className="flex w-12 flex-col items-center justify-center rounded border bg-white p-1">
-      <p>{count}</p>
+    <div className="flex w-12 flex-col items-center justify-center rounded border border-gray-800 p-1">
+      <p className="text-xl font-bold">{count}</p>
       <span className="text-[8px] uppercase">{measure}</span>
     </div>
   );
