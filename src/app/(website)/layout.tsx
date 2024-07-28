708a80038d9e96a44342cfce81921501e0000b2c
Đorđe Matić <djmatic@agfbl.org>
2024-07-28 08:36:19 +0200

    code refactor - create layout groups

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
-import "./globals.css";
+import "../globals.css";
 import Footer from "@/components/custom-ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
