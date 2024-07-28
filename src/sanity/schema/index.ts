71d8fdd5d5e7b82cb77b5e4d520ed4995661dc80
Đorđe Matić <djmatic@agfbl.org>
2024-07-28 13:15:14 +0200

    add tag schema

--- src/sanity/schema/index.ts
+++ src/sanity/schema/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 import categorySchema from "./category-schema";
+import tagSchema from "./tag-schema";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categorySchema],
+  types: [categorySchema, tagSchema],
 };
