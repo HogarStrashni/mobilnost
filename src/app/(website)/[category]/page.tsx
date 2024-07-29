65d2816ef0e76762c31ba4ae8de4ed4502a97ccd
Đorđe Matić <djmatic@agfbl.org>
2024-07-29 08:54:42 +0200

    small fix

--- src/app/(website)/[category]/page.tsx
+++ src/app/(website)/[category]/page.tsx
@@ -7,11 +7,12 @@ type CategoryPageProps = {
 };
 
 const CategoryPage = ({ params: { category } }: CategoryPageProps) => {
+  const title = category.replace("-", " ");
   return (
     <div className="h-full lg:border-r">
       <Projects
         num={10}
-        title={category}
+        title={title}
         className="grid-cols-1 pl-0 md:grid-cols-2 lg:pr-4"
         params={category}
       />
