0cbb4d5aae0fb16f9a995e254b9c803a1d5f8d25
Đorđe Matić <djmatic@agfbl.org>
2024-08-04 13:03:19 +0200

    use-active-link hook: improvements

--- src/lib/hooks/use-active-link.ts
+++ src/lib/hooks/use-active-link.ts
@@ -2,5 +2,5 @@ import { usePathname } from "next/navigation";
 
 export const useActiveLink = () => {
   const pathName = usePathname();
-  return (name: string | null) => pathName === "/" + name;
+  return (name: string | null) => pathName.startsWith("/" + name);
 };
