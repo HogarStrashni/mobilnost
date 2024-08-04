50910d48dc2ef5a54c366a3a0bab0314296851e6
Đorđe Matić <djmatic@agfbl.org>
2024-08-04 10:47:36 +0200

    implement crated components on page and template

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,50 +1,25 @@
+import CategorySection from "@/components/custom-ui/category-section";
 import { client } from "@/sanity/client";
 import { ARCTICLES_HOME_PAGE_QUERY } from "@/sanity/queries";
 import { ARCTICLES_HOME_PAGE_QUERYResult } from "@/sanity/types";
-import Link from "next/link";
 
 const Home = async () => {
-  const timeStart = Date.now();
   const data = await client.fetch<ARCTICLES_HOME_PAGE_QUERYResult>(
     ARCTICLES_HOME_PAGE_QUERY,
   );
-  const timeEnd = Date.now();
-  const time = timeEnd - timeStart;
-  console.log({ time });
-
-  const actualArticles = data
-    .map(({ category, slug: categorySlug, actualArticles }) =>
-      actualArticles.map((item) => ({ category, categorySlug, ...item })),
-    )
-    .flat();
 
   const newestArticles = data.map(({ actualArticles, ...rest }) => rest);
 
   return (
-    <div className="h-full lg:border-r">
-      {actualArticles.map((item) => (
-        <div key={item.slug} className="mt-12">
-          <Link href={`/${item.categorySlug}/${item.slug}` ?? "/"}>
-            {item.title}
-          </Link>
-        </div>
+    <div className="lg:border-r lg:pr-4">
+      {newestArticles.map(({ category, slug, newestArticles }) => (
+        <CategorySection
+          key={slug}
+          category={category}
+          slug={slug}
+          newestArticles={newestArticles}
+        />
       ))}
-      {newestArticles.map(
-        ({ category, slug: categorySlug, newestArticles }) => (
-          <div key={categorySlug} className="mt-12">
-            <Link href={`/${categorySlug}`}>{category}</Link>
-            <div>
-              {newestArticles.map((item) => (
-                <div key={item.slug} className="mt-4">
-                  <Link href={`/${categorySlug}/${item.slug}` ?? "/"}>
-                    {item.title}
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-        ),
-      )}
     </div>
   );
 };
