3c14fdb24b2bca7741d73505e35e6dfa5c9f520f
Đorđe Matić <djmatic@agfbl.org>
2024-08-15 19:18:07 +0200

    ....

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -8,18 +8,45 @@ const Home = async () => {
     ARCTICLES_HOME_PAGE_QUERY,
   );
 
+  // make two queries actual and newest
+  const actualArticles = data.map(({ newestArticles, ...rest }) => rest);
   const newestArticles = data.map(({ actualArticles, ...rest }) => rest);
 
+  const actualArticlesTransformed = actualArticles
+    .map(({ actualArticles, slug, category }) =>
+      actualArticles.map((article) => ({
+        categorySlug: slug,
+        category,
+        ...article,
+      })),
+    )
+    .flat();
+  const newestArticlesTransformed = newestArticles
+    .map(({ newestArticles, slug, category }) =>
+      newestArticles.map((article) => ({
+        categorySlug: slug,
+        category,
+        ...article,
+      })),
+    )
+    .flat();
+
   return (
-    <div className="lg:border-r lg:pr-4">
-      {newestArticles.map(({ category, slug, newestArticles }) => (
+    <div className="mx-auto my-6 min-h-96 max-w-screen-2xl px-2 sm:my-8 sm:px-5 lg:my-16 lg:px-12">
+      <section className="min-h-40 border">
+        <pre>{JSON.stringify(actualArticlesTransformed, null, 4)}</pre>
+      </section>
+      <section className="min-h-40 border">
+        <pre>{JSON.stringify(newestArticlesTransformed, null, 4)}</pre>
+      </section>
+      {/* {newestArticles.map(({ category, slug, newestArticles }) => (
         <CategorySection
           key={slug}
           category={category}
           slug={slug}
           newestArticles={newestArticles}
         />
-      ))}
+      ))} */}
     </div>
   );
 };
