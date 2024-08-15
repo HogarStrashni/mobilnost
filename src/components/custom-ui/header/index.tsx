92fdd1d851b3a6548240ace247665e34f8dbc34a
Đorđe Matić <djmatic@agfbl.org>
2024-08-15 16:40:50 +0200

    implement header with small refactor

--- src/components/custom-ui/header/index.tsx
+++ src/components/custom-ui/header/index.tsx
@@ -4,7 +4,7 @@ import { client } from "@/sanity/client";
 import { CATEGORY_QUERY } from "@/sanity/queries";
 import { CATEGORY_QUERYResult } from "@/sanity/types";
 
-import Logo from "@/components/custom-ui/logo";
+import Logo from "@/components/custom-ui/header/logo";
 import SocialMediaLinks from "@/components/custom-ui/social-media-links";
 import NavigationBar from "@/components/custom-ui/header/navigation-bar";
 import SideBar from "@/components/custom-ui/header/side-bar";
@@ -12,16 +12,20 @@ import SideBar from "@/components/custom-ui/header/side-bar";
 const Header = async () => {
   const allCategories =
     await client.fetch<CATEGORY_QUERYResult>(CATEGORY_QUERY);
+
   return (
-    <header className="bg-gray-100">
-      <div className="mx-auto flex min-h-16 max-w-screen-xl items-center justify-between gap-4 px-5 py-4 sm:py-6 md:px-8 lg:py-8">
+    <header className="font-oswald">
+      <section className="mx-auto flex max-w-screen-2xl items-center justify-between gap-4 px-2 py-4 sm:px-5 sm:py-6 lg:px-12">
         <Link href="/">
           <Logo />
         </Link>
         <SocialMediaLinks className="hidden lg:flex" />
         <SideBar allCategories={allCategories} />
-      </div>
-      <NavigationBar allCategories={allCategories} className="hidden lg:flex" />
+      </section>
+
+      <section className="hidden flex-col gap-5 text-gray-700 lg:flex">
+        <NavigationBar allCategories={allCategories} />
+      </section>
     </header>
   );
 };
--- src/components/custom-ui/social-media-links/index.tsx
+++ src/components/custom-ui/social-media-links/index.tsx
@@ -9,27 +9,27 @@ import SendEmailIcon from "@/components/custom-ui/social-media-links/icons/send-
 const allLinks = [
   {
     name: "facebook",
-    icon: <FacebookIcon className="size-6 lg:size-7" />,
+    icon: <FacebookIcon className="size-6" />,
     href: "https://www.facebook.com",
   },
   {
     name: "instagram",
-    icon: <InstagramIcon className="size-6 lg:size-7" />,
+    icon: <InstagramIcon className="size-6" />,
     href: "https://www.instagram.com",
   },
   {
     name: "linkedin",
-    icon: <LinkedinIcon className="size-6 lg:size-7" />,
+    icon: <LinkedinIcon className="size-6" />,
     href: "https://www.linkedin.com",
   },
   {
     name: "twitter",
-    icon: <TwitterIcon className="size-6 lg:size-7" />,
+    icon: <TwitterIcon className="size-6" />,
     href: "https://www.x.com",
   },
   {
     name: "email",
-    icon: <SendEmailIcon className="size-6 lg:size-7" />,
+    icon: <SendEmailIcon className="size-6" />,
     href: "mailto:milenko.dzever@gmail.com",
   },
 ];
@@ -40,7 +40,7 @@ type SocialMediaLinksProps = {
 
 const SocialMediaLinks = ({ className }: SocialMediaLinksProps) => {
   return (
-    <div className={cn("flex items-center gap-3 lg:gap-5", className)}>
+    <div className={cn("flex items-center gap-2", className)}>
       {allLinks.map(({ name, icon, href }) => (
         <a
           key={name}
