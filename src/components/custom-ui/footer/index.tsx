d84f54416280c28c4029a59f9ee645b59798577f
Đorđe Matić <djmatic@agfbl.org>
2024-08-15 16:40:10 +0200

    footer improvements

--- src/components/custom-ui/footer/index.tsx
+++ src/components/custom-ui/footer/index.tsx
@@ -1,8 +1,12 @@
 const Footer = () => {
+  const currentYear = new Date(Date.now()).getFullYear();
+
   return (
-    <footer className="flex flex-col items-center bg-gray-800 px-5 py-4 text-gray-200 md:px-8">
-      <span className="text-lg italic">mobilnost.ba</span>
-      <span className="text-xs">Copyright &#169; 2024 All right reserved!</span>
+    <footer className="flex flex-col items-center bg-gray-800 px-5 py-4 font-oswald text-gray-200 md:px-8">
+      <span className="text-center text-lg italic">mobilnost.ba</span>
+      <span className="text-center text-xs">
+        Copyright &#169; {currentYear} All right reserved!
+      </span>
     </footer>
   );
 };
