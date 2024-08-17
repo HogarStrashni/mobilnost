3507f48eb7e259af1168aa9b3aa41dd5abb65b96
Đorđe Matić <djmatic@agfbl.org>
2024-08-17 12:07:05 +0200

    implement newest-articles section (first iteration)

--- src/components/custom-ui/articles/newest-articles/index.tsx
+++ src/components/custom-ui/articles/newest-articles/index.tsx
@@ -1,11 +1,14 @@
 import { NEWEST_ARTICLES_QUERYResult } from "@/sanity/types";
+import CategorySection from "../category-section";
 
 type NewestArticlesProps = {
   articles: NEWEST_ARTICLES_QUERYResult;
 };
 
 const NewestArticles = ({ articles }: NewestArticlesProps) => {
-  return <pre>{JSON.stringify(articles, null, 4)}</pre>;
+  return articles.map(({ category, slug, articles }) => (
+    <CategorySection category={category} slug={slug} articles={articles} />
+  ));
 };
 
 export default NewestArticles;
