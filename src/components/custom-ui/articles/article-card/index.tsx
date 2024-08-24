1dfe54cb4ec29e8a15bc7e06481b262cecbed167
Đorđe Matić <djmatic@agfbl.org>
2024-08-24 12:51:07 +0200

    add folder article-card and fix imports

--- src/components/custom-ui/articles/article-card/index.tsx
+++ src/components/custom-ui/articles/article-card/index.tsx
@@ -7,37 +7,51 @@ import { ClockArrowUp, UserRoundPen } from "lucide-react";
 type ArticleCardProps = {
   article: ACTUAL_ARTICLES_QUERYResult[0];
   largeSize?: boolean;
+  hideExcerpt?: boolean;
 };
 
-const ArticleCard = ({ article, largeSize }: ArticleCardProps) => {
+const ArticleCard = ({
+  article,
+  largeSize,
+  hideExcerpt = false,
+}: ArticleCardProps) => {
   const { title, coverImage, author, excerpt, published, category, slug } =
     article;
 
   return (
-    <Link href={`${category?.slug}/${slug}`} className="flex flex-col gap-2">
+    <Link
+      href={`${category?.slug}/${slug}`}
+      className="flex flex-col gap-2 rounded bg-white p-4"
+    >
       {coverImage && (
         <Image
           src={urlFor(coverImage).url()}
           alt={coverImage.alt ?? ""}
           width={largeSize ? 1024 : 640}
           height={largeSize ? 640 : 400}
-          className="aspect-[1.6] w-full object-cover"
+          className="aspect-[1.6] w-full rounded object-cover"
           priority
         />
       )}
 
+      <div className="flex h-4 items-center">
+        <p className="text-xs">{category?.title ?? ""}</p>
+        <span className="mx-2 h-full border"></span>
+        <div className="flex items-center gap-1">
+          <ClockArrowUp className="size-3" />
+          <p className="text-xs">{new Date(published).toLocaleDateString()}</p>
+        </div>
+      </div>
+
       <div className="space-y-2">
         <h2 className="font-bold">{title}</h2>
-        <p className="line-clamp-3 text-sm">{excerpt}</p>
+
+        {!hideExcerpt && <p className="line-clamp-3 text-sm">{excerpt}</p>}
       </div>
       <div className="flex justify-between">
         <div className="flex items-center gap-2">
           <UserRoundPen className="size-4" />
           <p className="text-sm">{author}</p>
-        </div>
-        <div className="flex items-center gap-2">
-          <ClockArrowUp className="size-3" />
-          <p className="text-xs">{new Date(published).toLocaleDateString()}</p>
         </div>
       </div>
     </Link>
