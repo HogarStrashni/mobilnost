326e6e9e5d8216f386f422530e695b43738fd032
Đorđe Matić <djmatic@agfbl.org>
2024-08-28 06:53:27 +0200

    implement article category responsive design

--- src/app/(website)/[category]/layout.tsx
+++ src/app/(website)/[category]/layout.tsx
@@ -8,26 +8,20 @@ import CategorySectionTitle from "@/components/custom-ui/articles/category-secti
 
 type RootLayoutProps = Readonly<{
   children: ReactNode;
-  params: {
-    category: string;
-  };
 }>;
 
-const CategoryLayout = async ({
-  children,
-  params: { category },
-}: RootLayoutProps) => {
+const CategoryLayout = async ({ children }: RootLayoutProps) => {
   const actualArticles = await client.fetch<ACTUAL_ARTICLES_QUERYResult>(
     ACTUAL_ARTICLES_QUERY,
   );
 
   return (
-    <div className="mx-auto my-6 grid max-w-screen-2xl grid-cols-3 gap-4 sm:my-8 sm:px-5 lg:my-16 lg:px-12">
+    <div className="mx-auto my-8 grid min-w-64 max-w-screen-2xl gap-4 px-2 sm:px-4 md:px-8 lg:my-16 lg:grid-cols-3 lg:px-12">
       <section className="col-span-2">
         <div>{children}</div>
       </section>
 
-      <section className="pl-4">
+      <section className="hidden pl-4 lg:block">
         <CategorySectionTitle category="aktuelno" />
 
         <div className="grid items-start gap-4">
--- src/components/custom-ui/articles/article-card/index.tsx
+++ src/components/custom-ui/articles/article-card/index.tsx
@@ -58,7 +58,7 @@ const ArticleCard = ({
           <h2
             className={cn(
               "font-lora text-lg font-bold leading-6 text-gray-700 sm:text-xl",
-              largeSize ? "mb-4 sm:text-3xl lg:text-4xl" : "mb-2",
+              largeSize ? "mb-4 sm:text-3xl md:text-4xl" : "mb-2",
             )}
           >
             {title}
@@ -68,7 +68,7 @@ const ArticleCard = ({
             <p
               className={cn(
                 "line-clamp-3 font-roboto text-sm text-gray-600",
-                largeSize ? "lg:text-base" : "text-sm",
+                largeSize ? "md:text-base" : "text-sm",
               )}
             >
               {excerpt}
