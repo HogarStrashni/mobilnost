cc6638345a397a3c575b2ce5a2f65f87561bab3e
Đorđe Matić <djmatic@agfbl.org>
2024-08-31 08:43:09 +0200

    remove share button from article card footer

--- src/components/custom-ui/articles/article-card/article-card-footer.tsx
+++ src/components/custom-ui/articles/article-card/article-card-footer.tsx
@@ -1,10 +1,8 @@
 import { ArrowRight } from "lucide-react";
-import ArticleShareButton from "@/components/custom-ui/articles/article-share-button";
 
 const ArticleCardFooter = () => {
   return (
     <div className="relative font-oswald text-gray-500">
-      <ArticleShareButton />
       <button className="ml-auto flex items-center gap-1 rounded border border-gray-100 px-2 py-0.5 text-sm duration-300 ease-out hover:bg-gray-50">
         <span className="pb-px">Opširnije</span>
         <ArrowRight className="size-3" />
