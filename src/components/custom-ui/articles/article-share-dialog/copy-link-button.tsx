2c84ba0e9c85fc01fce732eec7924707540d9460
Đorđe Matić <djmatic@agfbl.org>
2024-08-31 21:28:59 +0200

    implement copy link to clipboard

--- src/components/custom-ui/articles/article-share-dialog/copy-link-button.tsx
+++ src/components/custom-ui/articles/article-share-dialog/copy-link-button.tsx
@@ -1,4 +1,5 @@
-import { Link } from "lucide-react";
+import { useState } from "react";
+import { Check, Link } from "lucide-react";
 import SocialIcon from "@/components/custom-ui/articles/article-share-dialog/social-icon";
 
 type CopyLinkButtonProps = {
@@ -6,10 +7,25 @@ type CopyLinkButtonProps = {
 };
 
 const CopyLinkButton = ({ articleLink }: CopyLinkButtonProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyLink = async () => {
+    await navigator.clipboard.writeText(articleLink);
+    setIsCopied(true);
+  };
+
   return (
-    <button className="mx-auto max-w-fit">
-      <SocialIcon title="Kopiraj link">
-        <Link className="size-4 sm:size-5" />
+    <button
+      className="mx-auto max-w-fit disabled:pointer-events-none"
+      onClick={handleCopyLink}
+      disabled={isCopied}
+    >
+      <SocialIcon title={isCopied ? "Link kopiran" : "Kopiraj link"}>
+        {isCopied ? (
+          <Check className="size-4 sm:size-5" />
+        ) : (
+          <Link className="size-4 sm:size-5" />
+        )}
       </SocialIcon>
     </button>
   );
