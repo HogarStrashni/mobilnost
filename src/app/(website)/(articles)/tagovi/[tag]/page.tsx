c8e90fb4701c8111a38650104e547c1039b1beeb
Đorđe Matić <djmatic@agfbl.org>
2024-09-01 08:52:26 +0200

    small code refactor and implement page-not-found fallback

--- src/app/(website)/(articles)/tagovi/[tag]/page.tsx
+++ src/app/(website)/(articles)/tagovi/[tag]/page.tsx
@@ -1,9 +1,9 @@
 import ArticleCard from "@/components/custom-ui/articles/article-card";
 import CategorySectionTitle from "@/components/custom-ui/articles/category-section/category-section-title";
+import PageNotFound from "@/components/custom-ui/articles/page-not-found";
 import { client } from "@/sanity/client";
 import { ARTICLES_BY_TAG_QUERY, TAGS_QUERY } from "@/sanity/queries";
 import { ARTICLES_BY_TAG_QUERYResult, TAGS_QUERYResult } from "@/sanity/types";
-import { Frown } from "lucide-react";
 
 type TagPageProps = {
   params: {
@@ -11,30 +11,22 @@ type TagPageProps = {
   };
 };
 
-const TagPage = async ({ params }: TagPageProps) => {
-  const currentTag = params.tag;
-
+const TagPage = async ({ params: { tag } }: TagPageProps) => {
   const allTags = await client.fetch<TAGS_QUERYResult>(TAGS_QUERY);
-  const tagSlugs = allTags.map((tag) => tag.slug);
 
   // minimize database queries with early return
-  if (!tagSlugs.includes(currentTag)) {
-    return (
-      <div className="flex items-center justify-center gap-2 rounded bg-white py-4 font-oswald shadow-sm sm:text-lg lg:text-xl">
-        Stranica nije pronađena!
-        <Frown className="size-5 lg:size-6" />
-      </div>
-    );
+  if (!allTags.some((t) => t.slug === tag)) {
+    return <PageNotFound>Stranica nije pronađena!</PageNotFound>;
   }
 
   const articlesByTag = await client.fetch<ARTICLES_BY_TAG_QUERYResult>(
     ARTICLES_BY_TAG_QUERY,
-    { currentTag },
+    { currentTag: tag },
   );
 
   return (
     <div>
-      <CategorySectionTitle category={currentTag} hashTag />
+      <CategorySectionTitle category={tag} hashTag />
 
       <div className="grid gap-4 sm:grid-cols-2">
         {articlesByTag.map((article) => {
