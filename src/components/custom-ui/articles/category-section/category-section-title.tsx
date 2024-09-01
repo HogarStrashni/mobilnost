36ba67478b16e8d86b7793faa434b1ec31559407
Đorđe Matić <djmatic@agfbl.org>
2024-09-01 07:58:38 +0200

    category-section-title: add conditionaly hash icon

--- src/components/custom-ui/articles/category-section/category-section-title.tsx
+++ src/components/custom-ui/articles/category-section/category-section-title.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
-import { FolderOpenDot } from "lucide-react";
+import { FolderOpenDot, Hash } from "lucide-react";
 
 type CategorySectionTitleProps = {
   category: string;
   href?: string;
+  hashTag?: boolean;
 };
 
 const CategorySectionTitle = ({
   href,
   category,
+  hashTag = false,
 }: CategorySectionTitleProps) => {
   return href ? (
     <Link
@@ -22,7 +24,11 @@ const CategorySectionTitle = ({
     </Link>
   ) : (
     <div className="mx-2 mb-4 flex max-w-fit items-center gap-2 font-bold uppercase sm:mx-0 sm:mb-6">
-      <FolderOpenDot className="size-4 text-green-primary sm:size-6" />
+      {hashTag ? (
+        <Hash className="size-4 text-green-primary sm:size-6" />
+      ) : (
+        <FolderOpenDot className="size-4 text-green-primary sm:size-6" />
+      )}
       <span className="font-oswald text-lg text-purple-primary sm:text-2xl">
         {category}
       </span>
