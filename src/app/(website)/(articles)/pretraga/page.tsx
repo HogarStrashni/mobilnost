81cf618836af54debf33925dfeaf15b3f75e6bf4
Đorđe Matić <djmatic@agfbl.org>
2024-09-01 12:53:30 +0200

    '/pretraga' route: add search-form-section and implement early returns

--- src/app/(website)/(articles)/pretraga/page.tsx
+++ src/app/(website)/(articles)/pretraga/page.tsx
@@ -1,5 +1,34 @@
-const SearchPage = () => {
-  return <div>PRETRAGA PLACEHOLDER</div>;
+import SearchFormSection from "@/components/custom-ui/search-form-section";
+
+type SearchPageProps = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const searchParamQ = searchParams.q;
+
+  if (!searchParamQ || typeof searchParamQ !== "string")
+    return (
+      <SearchFormSection
+        searchValue=""
+        message="Termin mora imati minimalno 3 karaktera."
+      />
+    );
+
+  if (searchParamQ.length < 3)
+    return (
+      <SearchFormSection
+        searchValue={searchParamQ}
+        message="Termin mora imati minimalno 3 karaktera."
+      />
+    );
+
+  return (
+    <SearchFormSection
+      searchValue={searchParamQ}
+      message={`Ukupno 5 rezultata pretrage: "${searchParamQ}"`}
+    />
+  );
 };
 
 export default SearchPage;
