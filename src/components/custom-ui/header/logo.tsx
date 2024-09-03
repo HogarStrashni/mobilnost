86ac20e0fbd3ca0040fe049508a8c950bdbab9c4
Đorđe Matić <djmatic@agfbl.org>
2024-09-03 09:09:03 +0200

    implement logo bigger text

--- src/components/custom-ui/header/logo.tsx
+++ src/components/custom-ui/header/logo.tsx
@@ -9,7 +9,7 @@ const Logo = () => {
         alt="logo"
         className="block w-16 rounded sm:w-20 lg:w-24"
       />
-      <span className="bg-gradient-to-br from-green-primary to-purple-primary bg-clip-text pr-2 text-2xl font-medium italic text-transparent max-[360px]:hidden sm:text-3xl lg:text-4xl">
+      <span className="bg-gradient-to-br from-green-primary to-purple-primary bg-clip-text pr-2 text-3xl font-medium italic text-transparent max-[390px]:hidden sm:text-5xl lg:text-7xl">
         mobilnost.ba
       </span>
     </div>
